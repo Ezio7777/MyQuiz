280bfda93e6552f802faa3989ac4eef110392b6f
guptasoumya22 <100026401+guptasoumya22@users.noreply.github.com>
2023-08-21 17:48:07 +0530

    upgrading contact_us page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
-import Navbar from "./components/navbar"
-import Footer from "./components/footer.jsx"
-import Home from "./components/home.jsx"
-import Login from "./components/login.jsx"
-import Signup from "./components/signup"
+import Navbar from "./components/navbar";
+import Footer from "./components/footer.jsx";
+import Home from "./components/home.jsx";
+import Login from "./components/login.jsx";
+import Signup from "./components/signup";
 import Join_code from "./components/join_code.jsx";
-import Host_quiz from "./components/host_quiz"
+import Host_quiz from "./components/host_quiz";
 import Share_code from "./components/share_code";
 import Q_add from "./components/q_add";
+import Contact_us from "./components/contact_us";
 
-import "./App.css"
-
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Host_quiz/>
-      <Footer/>
+      <Navbar />
+      <Contact_us />
+      <Footer />
     </div>
   );
 }
--- src/components/contact_us.jsx
+++ src/components/contact_us.jsx
@@ -1,48 +1,49 @@
-import React from 'react'
-import "../components_style/contact_us.css"
+import React from "react";
+import "../components_style/contact_us.css";
 
 const contact_us = () => {
-  
-
-    return (
-        
-        <div id="overlay">
-            <div class="footer-icons">
-        <a href="https://github.com/guptasoumya22">
-            <i class="fab fa-github"></i>
+  return (
+    <div className="container contact_us_body">
+      {/* icons */}
+      <div class="footer-icons">
+        <a href="https://github.com/Ezio7777/Be-Quick__">
+          <i class="fab fa-github"></i>
         </a>
+
         <a href="https://www.instagram.com/soumyaa__22">
-            <i class="fab fa-instagram"></i>
+          <i class="fab fa-instagram"></i>
         </a>
         <a href="https://www.linkedin.com/in/soumya-gupta-522a02237">
-            <i class="fab fa-linkedin"></i>
+          <i class="fab fa-linkedin"></i>
         </a>
         <a href="https://www.facebook.com/">
-            <i class="fab fa-facebook"></i>
+          <i class="fab fa-facebook"></i>
         </a>
-    </div>
-        <form>
-            <h1>Contact Us</h1>
-            <label for="name">Name:</label>
-            <input type="text" id="name" placeholder="Your name"></input>
-            <small className="error"></small>
+      </div>
 
-            <label for="name">Email:</label>
-            <input type="text" id="email" placeholder="Your email"></input>
-            <small className="error"></small>
-
-            <label for="name">Message:</label>
-            <textarea id="message" placeholder="Your message" rows="6"></textarea>
-            <small className="error"></small>
-
-            <div className="center">
-            <input type="submit" value="Send Message"></input>
-            <p id="Success"></p>
-            </div>
+      {/* Form */}
+      <div className="form-container">
+        <p className="title">Contact Us</p>
+        <form className="form">
+          <div className="input-group">
+            <label for="username">Email</label>
+            <input type="text" name="username" id="username" placeholder="" />
+          </div>
+          <div className="input-group">
+            <label for="password">Message</label>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              className="message"
+              placeholder=""
+            />
+          </div>
+          <button className="sign">Send Message</button>
         </form>
+      </div>
     </div>
-    
-    )
-}
+  );
+};
 
-export default contact_us
+export default contact_us;
