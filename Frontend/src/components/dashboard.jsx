035f82571b18e73a505b9162afe9501f8b8b0d81
guptasoumya22 <100026401+guptasoumya22@users.noreply.github.com>
2023-11-03 19:03:31 +0530

    adding dashboard

---
+++ Frontend/src/components/dashboard.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "../components_style/dashboard.css";
+
+const Dashboard = () => {
+  const [selectedOption, setSelectedOption] = useState("join");
+
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div className="dashboard-container container">
+      <div className="option-section">
+        <div
+          className={`option ${selectedOption === "join" ? "selected" : ""}`}
+          onClick={() => handleOptionChange("join")}
+        >
+          Join
+        </div>
+        <div
+          className={`option ${selectedOption === "host" ? "selected" : ""}`}
+          onClick={() => handleOptionChange("host")}
+        >
+          Host
+        </div>
+      </div>
+
+      <div className="info-section">
+        {selectedOption === "join" ? (
+          <div className="join-info">
+            <div className="column">
+              <div className="info-title">Quiz Name</div>
+              <div className="info-detail">OSS</div>
+              <div className="info-detail">AP</div>
+            </div>
+            <div className="column">
+              <div className="info-title">No. of Questions</div>
+              <div className="info-detail">10</div>
+              <div className="info-detail">20</div>
+            </div>
+            <div className="column">
+              <div className="info-title">Total Points</div>
+              <div className="info-detail">20</div>
+              <div className="info-detail">40</div>
+            </div>
+          </div>
+        ) : (
+          <div className="host-info">
+            <div className="column">
+              <div className="info-title">Quiz Name</div>
+              <div className="info-detail">OSS</div>
+              <div className="info-detail">AP</div>
+            </div>
+            <div className="column">
+              <div className="info-title">No. of Students</div>
+              <div className="info-detail">50</div>
+              <div className="info-detail">60</div>
+            </div>
+            <div className="column">
+              <div className="info-title">Duration</div>
+              <div className="info-detail">30 minutes</div>
+              <div className="info-detail">50 minutes</div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
